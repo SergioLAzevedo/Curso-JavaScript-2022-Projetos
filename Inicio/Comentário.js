cc2f2c8780407670512d27e7b1b2b1f3594f4775
Sérgio Lourenço <sergiolourencodeazevedo@gmail.com>
2022-06-13 20:49:21 -0300

    inicio do curso de JS

---
+++ Inicio/Comentário.js
@@ -0,0 +1,12 @@
+//console.log é usado para exibir algo na tela do computador 
+console.log ("Passo 3");
+console.log ("Passo 1"); // mais um exemplo de console.log
+//console.log ("Passo 2");
+
+/*
+* Esse
+* é um comentário
+* de multiplas 
+* linhas !
+*/
+console.log ("Passo 2")
