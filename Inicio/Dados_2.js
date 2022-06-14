466306ce8baec05a441b86aaa6b8ad6bc9c1c351
Sérgio Lourenço <sergiolourencodeazevedo@gmail.com>
2022-06-14 14:28:27 -0300

    ok

---
+++ Inicio/Dados_2.js
@@ -0,0 +1,14 @@
+let preco = 19.90;
+let desconto = 0.4;
+
+console.log(19.9 * 0.6);
+
+let precoComDesconto = (preco * (1 - desconto));
+console.log (precoComDesconto);
+
+let nome = "Caderno"; // String (Texto) -> sequencia de símmbolos 
+let categoria = "Papelaria" ;
+console.log("Produto: "+ nome 
++",Categoria:"+categoria 
++",preço:"+ preco
++",Desconto:" + desconto )
---
+++ Inicio/Desafio_Area.js
@@ -0,0 +1,12 @@
+const PI = 3.141592;
+const raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("Resultado do área é :" + areaCirc +" m2" );
+
+// poderia usar no lugar da constante PI usar o proprio pi 
+//que usaria console.log(Math.PI)
+
+areaCirc = Math.PI * raio * raio;
+console.log("Resultado do área é :" + areaCirc +" m2" );
+
