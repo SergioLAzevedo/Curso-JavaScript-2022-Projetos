7b2905692208a9015da91d75d3939a0de10c29fc
Sérgio Lourenço <sergiolourencodeazevedo@gmail.com>
2022-08-27 23:35:28 -0300

    Objeto 2.0

--- Fundamentos/Objeto.js
+++ Fundamentos/Objeto.js
@@ -6,8 +6,8 @@ prod1['Desconto Legal'] = 0.40 //Evitar atributos com espaço
 console.log(prod1)
 
 const prod2 = {
-        nome: 'Camisa Polo'  
-        preco: 79.90 
+nome: 'Camisa Polo',  
+preco: 79.90 
 
 
 }
