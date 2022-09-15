1f89db686bfe83bf44b347f67b0f3ab8e7f61cf0
Sérgio Lourenço <sergiolourencodeazevedo@gmail.com>
2022-09-15 10:33:00 -0300

    Hoisting

---
+++ Fundamentos/Hoisting.js
@@ -0,0 +1,11 @@
+console.log('a =',a)
+var a = 2
+console.log ('a =',a)
+
+
+// COM LET Dá erro, já com VAR a variavél é jogada pra cima 
+// console.log('b =', b)
+// let b = 2
+// console.log('b =',b)
+
+
